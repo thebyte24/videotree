559fe5755bc98d410371ea40251013a8a958b579
CharanKuravi <collegemail36@gmail.com>
2026-05-30 14:33:50 +0530

    fix: use DB_PASSWORD env var for GoDaddy hosted database

--- server/db.js
+++ server/db.js
@@ -2,8 +2,9 @@ const mysql = require('mysql2/promise')
 
 const pool = mysql.createPool({
   host:     process.env.DB_HOST,
+  port:     process.env.DB_PORT || 3306,
   user:     process.env.DB_USER,
-  password: process.env.DB_PASS,
+  password: process.env.DB_PASSWORD || process.env.DB_PASS,
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
